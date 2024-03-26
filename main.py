fadf4fd92f9b52935162221d4b73c3ee9dc57804
Kartsev Ivan <1ivan1laba1@gmail.com>
2024-03-26 23:04:52 +0300

    1

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame as pg
+import pygame.font
+
 
 class Render:
     def __init__(self):
@@ -8,12 +10,39 @@ class Render:
         self.FPS = 60
         self.screen = pg.display.set_mode(self.RES)
         self.clock = pg.time.Clock()
+        self.font = pygame.font.SysFont("Arial", 14)
 
     def draw(self):
         self.screen.fill(pg.Color('Black'))
-        # pg.draw.circle(self.screen, pg.Color("white"), [200, 200], 200)
-        pg.draw.line(self.screen, pg.Color("white"), [100, 100], [200, 200])
-        pg.draw.rect(self.screen, pg.Color("white"), [100, 200, 200, 200], 1, 50)
+        # Пространство графика
+        pg.draw.rect(self.screen, pg.Color("#0f0f0f"), [200, 50, 950, 620])
+
+        # Горизонтальная линия
+        pg.draw.line(self.screen, pg.Color("white"), [200, 670], [1150, 670])
+
+        # Вертикальная линия
+        pg.draw.line(self.screen, pg.Color("white"), [200, 50], [200, 675])
+
+        # Конечная линия
+        pg.draw.line(self.screen, pg.Color("white"), [1150, 680], [1150, 660])
+        pg.draw.line(self.screen, pg.Color("#2c2c2c"), [1150, 50], [1150, 660])
+        text = self.font.render(str(int(minutes)), False, pg.Color("white"), None)
+        self.screen.blit(text, [1145, 690])
+
+        # Линейка
+        for i in range(1, int(minutes)):
+            cur = 200 + (950 / int(minutes)) * i
+            pg.draw.line(self.screen, pg.Color("white"), [cur, 675], [cur, 665])
+
+        for i in range(0, int((int(minutes) / 10)) + 1):
+            cur = 200 + (950 / (int(minutes) / 10)) * i
+            pg.draw.line(self.screen, pg.Color("white"), [cur, 680], [cur, 660])
+            pg.draw.line(self.screen, pg.Color("#2c2c2c"), [cur, 50], [cur, 660])
+            if i == 0:
+                text = self.font.render("0", False, pg.Color("white"), None)
+            else:
+                text = self.font.render(str(i) + "0", False, pg.Color("white"), None)
+            self.screen.blit(text, [cur - 5, 690])
 
     def run(self):
         while True:
@@ -24,5 +53,6 @@ class Render:
             self.clock.tick(self.FPS)
 
 if __name__ == '__main__':
+    minutes = input("введите количество минут: ")
     app = Render()
     app.run()
